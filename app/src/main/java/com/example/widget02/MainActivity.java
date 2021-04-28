673c0d3c91762bd580e5f156f5c769e05fd60f94
남승균 <skyun.nam@gmail.com>
2021-04-28 14:15:03 +0900

    RadioGroup, CheckBox Event 처리

--- app/src/main/java/com/example/widget02/MainActivity.java
+++ app/src/main/java/com/example/widget02/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.widget02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -24,5 +25,32 @@ public class MainActivity extends AppCompatActivity {
         checkBox = findViewById(R.id.checkBox);
         radioChecker = findViewById(R.id.radioChecker);
         checkboxChecker = findViewById(R.id.checkboxChecker);
+
+        //  RadioGroup -> OnCheckedChange
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //  선택된 RadioButton의 아이디가 checkedId로 전달
+                switch(checkedId) {
+                    case R.id.radio1:
+                        radioChecker.setText("Female이 선택되었습니다.");
+                        break;
+                    case R.id.radio2:
+                        radioChecker.setText("Male이 선택되었습니다.");
+                }
+            }
+        });
+
+        //  CheckBox -> OnClickListener 이벤트 발생
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  다운 캐스팅
+                CheckBox cb = (CheckBox)v;
+                boolean checked = cb.isChecked();    //  체크 여부 확인
+
+                checkboxChecker.setText("CheckBox is " + (checked ? "checked": "unchecked"));
+            }
+        });
     }
 }
